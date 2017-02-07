c6cea68d641fd6ad4c8fcfa26227806f525b6ab5
David Liang <davidliangx27@gmail.com>
2017-02-07 17:43:40 -0600

    New suggestions bar

--- ui.js
+++ ui.js
@@ -2,6 +2,7 @@ goog.require('goog.dom');
 goog.require('SpeechBlocks.WorkspaceStates');
 goog.require('SpeechGames');
 
+
 $(document).ready(function() {
 
 var title = goog.dom.$('levels')
@@ -23,57 +24,18 @@ for (var i = 0; i < SpeechGames.MAX_LEVEL; i++) {
   goog.dom.appendChild(title, goog.dom.createTextNode(" "));
 }
 
-var page = 0;
-var textIndex = -1;
-var manual = [
-  [
-    ["Adding blocks:"], // title
-    ["Add a move block", "Add a turn block", "Add a repeat block"] // text
-  ],
-  [
-    ["Putting blocks:"],
-    ["Put block 1 before block 2", "Put block 1 after block 2"]
-  ],
-  [
-    ["Changing blocks:"],
-    ["Change the first field in block 1 to left", "Change the second field in block 2 to 1000"]
-  ],
-  [
-    ["Running the program:"],
-    ["Run the program"]
-  ]
-]
-
-var displayManualPage = function(index) {
-  if (index >= 0 && index < manual.length)
-  {
-    page = index;
-  }
-}
-
-var advanceManualPage = function()
-{
-  displayManualPage((page + 1) % manual.length);
-}
-
-var updateManual = function() {
-  textIndex = (textIndex + 1) % manual[page][1].length;
-  $("#manual-title").text(manual[page][0][0]);
-  $("#manual-text").text("\"" + manual[page][1][textIndex] + "\" ").fadeIn().delay(2000).fadeOut(200, updateManual);
-}
-
-updateManual();
-
+var sug = new SpeechBlocks.Suggestions();
+sug.setSuggestions(["add"]);
 SpeechGames.controller.addStateChangeListener(function(state) {
   switch (state) {
     case SpeechBlocks.WorkspaceStates.EMPTY:
-      displayManualPage(0);
+      sug.setSuggestions(["add"]);
       break;
     case SpeechBlocks.WorkspaceStates.ALL_BLOCKS_CONNECTED:
-      displayManualPage(2);
+      sug.setSuggestions(["add","change","delete","run"]);
       break;
     case SpeechBlocks.WorkspaceStates.BLOCKS_UNCONNECTED:
-      displayManualPage(1);
+      sug.setSuggestions(["add","change","delete","run","put"]);
       break;
     default:
       console.log('Unknown state! Cannot update manual.');
