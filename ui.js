cbfc8b72c81608e6e1b9289c3bb14645b8ae92bf
David Liang <davidliangx27@gmail.com>
2017-02-07 14:02:33 -0600

    Brought back error display, restructured manual DS

--- ui.js
+++ ui.js
@@ -20,33 +20,43 @@ for (var i = 0; i < SpeechGames.MAX_LEVEL; i++) {
   goog.dom.appendChild(title, goog.dom.createTextNode(" "));
 }
 
-var manualPageIndex = 0;
+var page = 0;
 var textIndex = -1;
-
-var manualTitles = ["Adding blocks:", "Putting blocks:", "Changing blocks:", "Running the program:"];
-var manualPages = [
-  ["Add a move block", "Add a turn block", "Add a repeat block"],
-  ["Put block 1 before block 2", "Put block 1 after block 2"],
-  ["Change the first  field in block 1 to left", "Change the second field in block 2 to 1000"],
-  ["Run the program"]
+var manual = [
+  [
+    ["Adding blocks:"], // title
+    ["Add a move block", "Add a turn block", "Add a repeat block"] // text
+  ],
+  [
+    ["Putting blocks:"],
+    ["Put block 1 before block 2", "Put block 1 after block 2"]
+  ],
+  [
+    ["Changing blocks:"],
+    ["Change the first field in block 1 to left", "Change the second field in block 2 to 1000"]
+  ],
+  [
+    ["Running the program:"],
+    ["Run the program"]
+  ]
 ]
 
 var displayManualPage = function(index) {
-  if (index >= 0 && index < manualPages.length)
+  if (index >= 0 && index < manual.length)
   {
-    manualPageIndex = index;
+    page = index;
   }
 }
 
 var advanceManualPage = function()
 {
-  displayManualPage((manualPageIndex + 1) % manualPages.length);
+  displayManualPage((page + 1) % manual.length);
 }
 
 var updateManual = function() {
-  textIndex = (textIndex + 1) % manualPages[manualPageIndex].length;
-  $("#manual-title").text(manualTitles[manualPageIndex]);
-  $("#manual-text").text("\"" + manualPages[manualPageIndex][textIndex] + "\" ").fadeIn().delay(1000).fadeOut(200, updateManual);
+  textIndex = (textIndex + 1) % manual[page][1].length;
+  $("#manual-title").text(manual[page][0][0]);
+  $("#manual-text").text("\"" + manual[page][1][textIndex] + "\" ").fadeIn().delay(2000).fadeOut(200, updateManual);
 }
 
 updateManual();
