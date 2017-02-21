3a4d95fa3400a9875a369b9a9a6d6a9cd859afcd
David Liang <davidliangx27@gmail.com>
2017-02-21 14:49:45 -0600

    Closure commenting, rearrangement, renaming

--- speech.js
+++ speech.js
@@ -1,8 +1,9 @@
 /**
  * @fileoverview Driver script for speech processing.
- * @author aravart@cs.wisc.edu (Ara Vartanian)
+ * @author aravart@cs.wisc.edu (Ara Vartanian), dliang@wisc.edu (David Liang)
  */
 goog.provide('SpeechGames');
+goog.provide('SpeechGames.Speech');
 
 goog.require('Blockly.Workspace');
 goog.require('SpeechBlocks.Controller');
@@ -22,31 +23,10 @@ SpeechGames.controller = null;
 SpeechGames.workspace = null;
 
 /**
- * Extracts a parameter from the URL.
- * If the parameter is absent default_value is returned.
- * @param {string} name The name of the parameter.
- * @param {string} defaultValue Value to return if paramater not found.
- * @return {string} The parameter value or the default value if not found.
+ * A global reference to the interpreter.
+ * @public {Blockly.Interpreter}
  */
-SpeechGames.getStringParamFromUrl = function(name, defaultValue) {
-  var val =
-      window.location.search.match(new RegExp('[?&]' + name + '=([^&]+)'));
-  return val ? decodeURIComponent(val[1].replace(/\+/g, '%20')) : defaultValue;
-};
-
-/**
- * Extracts a numeric parameter from the URL.
- * If the parameter is absent or less than min_value, min_value is
- * returned.  If it is greater than max_value, max_value is returned.
- * @param {string} name The name of the parameter.
- * @param {number} minValue The minimum legal value.
- * @param {number} maxValue The maximum legal value.
- * @return {number} A number in the range [min_value, max_value].
- */
-SpeechGames.getNumberParamFromUrl = function(name, minValue, maxValue) {
-  var val = Number(SpeechGames.getStringParamFromUrl(name, 'NaN'));
-  return isNaN(val) ? minValue : goog.math.clamp(minValue, val, maxValue);
-};
+SpeechGames.interpreter = null;
 
 /**
  * Maximum number of levels.  Common to all apps.
@@ -58,14 +38,14 @@ SpeechGames.MAX_LEVEL = 12;
  * User's current level (e.g. 5).
  * @public
  */
-SpeechGames.LEVEL =
-    SpeechGames.getNumberParamFromUrl('level', 1, SpeechGames.MAX_LEVEL);
+SpeechGames.LEVEL = null;
 
 /**
  * Bind a function to a button's click event.
  * On touch-enabled browsers, ontouchend is treated as equivalent to onclick.
  * @param {!Element|string} el Button element or ID thereof.
  * @param {!Function} func Event handler to bind.
+ * @public
  */
 SpeechGames.bindClick = function(el, func) {
   if (typeof el == 'string') {
@@ -75,60 +55,12 @@ SpeechGames.bindClick = function(el, func) {
   el.addEventListener('touchend', func, true);
 };
 
-// Initialize microphone and speech handling.
-$(document).ready(function() {
-  SpeechGames.speech = new SpeechGames.Speech();
-  SpeechGames.controller = SpeechBlocks.Controller.injectIntoDiv('blocklyDiv', {
-    media: 'lib/google-blockly/media/',
-    trashcan: false,
-    scrollbars: false,
-    toolbox: document.getElementById('toolbox')
-  });
-  SpeechGames.interpreter = new SpeechBlocks.Interpreter(SpeechGames.controller);
-  SpeechGames.workspace = SpeechGames.controller.workspace_;
 
-  if(!getParameterByName('debug')) {
-    $('#debug').hide();
-  }
-
-  if(!getParameterByName('demo')) {
-    SpeechGames.speech.awake = true;
-  } else {
-    SpeechGames.speech.demoMode = true;
-    SpeechGames.speech.awake = false;
-    SpeechGames.speech.setMicInterval();
-    console.log("demo mode");
-  }
-  if (getParameterByName('level')) {
-    SpeechGames.speech.awake = true;
-  }
-   $('#q')
-    .change(SpeechGames.speech.scheduleParse.bind(SpeechGames.speech))
-    .mousedown(SpeechGames.speech.scheduleParse.bind(SpeechGames.speech))
-    .mouseup(SpeechGames.speech.scheduleParse.bind(SpeechGames.speech))
-    .click(SpeechGames.speech.scheduleParse.bind(SpeechGames.speech))
-    .keydown(SpeechGames.speech.scheduleParse.bind(SpeechGames.speech))
-    .keyup(SpeechGames.speech.scheduleParse.bind(SpeechGames.speech))
-    .keypress(SpeechGames.speech.scheduleParse.bind(SpeechGames.speech));
-
-  $('#microphone')
-      .click(SpeechGames.speech.toggleDictation.bind(SpeechGames.speech));
-  if (SpeechGames.speech.demoMode && !SpeechGames.speech.awake) {
-     $("#user-message").hide().text("Say 'Hey Jerry' to wake me up.").fadeIn(500);
-  } else {
-    $("#user-message").hide().text("Awaiting your command!").fadeIn(500);
-  }
-
-  // $('#runButton').on('click', run);
-  // $('#showButton').on('click', showCode);
-
-  $('#debugButton').on('click', function() { 
-    $('#debug').toggle();
-  });
-  $('#buttonRow').hide();
-  $('#levelDescription').text(Turtle.descriptions[SpeechGames.LEVEL]);
-});
-
+/**
+ * Instantiates a speech object, used for controlling speech input to interpreter.
+ * @public
+ * @constructor
+ */
 SpeechGames.Speech = function() {
     this.oldQ = null;
     this.parseTimer = null;
@@ -145,7 +77,11 @@ SpeechGames.Speech = function() {
     this.parseTimer = null;
 };
 
-SpeechGames.Speech.prototype.setMicInterval = function() {
+/**
+ * Sets the interval that ensures the mic is lietning (when applicable).
+ * @private
+ */
+SpeechGames.Speech.prototype.setMicInterval_ = function() {
   if (!window.location.origin.includes("file")) {
     if (this.interval) {
       clearInterval(this.interval);
@@ -153,7 +89,7 @@ SpeechGames.Speech.prototype.setMicInterval = function() {
     this.interval = setInterval(function() {
       if (!this.listening) {
         this.listening = true;
-        this.startDictation(); 
+        this.startDictation_(); 
       } 
     }.bind(this), 100);
   } else {
@@ -161,7 +97,14 @@ SpeechGames.Speech.prototype.setMicInterval = function() {
   }
 };
 
-SpeechGames.Speech.prototype.speechCorrections = function(speech) {
+/**
+ * Hard replacements for certain phrases. 
+ * @param {string} speech Utterance to be corrected.
+ * @return {string} The corrected utterance.
+ * @private
+ */
+SpeechGames.Speech.prototype.speechCorrections_ = function(speech) {
+  //TODO: Perhaps replace this with a better system, possibly by finding nearest neighbor?
   speech = speech.toLowerCase();
   speech = speech.replace(/\bqueens\b/, 'please');
   speech = speech.replace(/\bfreeze out\b/, 'please add');
@@ -202,7 +145,11 @@ SpeechGames.Speech.prototype.speechCorrections = function(speech) {
   return speech;
 };
 
-SpeechGames.Speech.prototype.startDictation = function() {
+/**
+ * Restarts the listening for a new utterance.
+ * @private
+ */
+SpeechGames.Speech.prototype.startDictation_ = function() {
   if (window.hasOwnProperty('webkitSpeechRecognition')) {
     if (this.recognition) {
       this.recognition.stop();
@@ -223,10 +170,10 @@ SpeechGames.Speech.prototype.startDictation = function() {
 
     this.recognition.onresult = function(e) {
       var unfiltered = e.results[0][0].transcript;
-      var corrections = this.speechCorrections(unfiltered);
+      var corrections = this.speechCorrections_(unfiltered);
       $("#q").val(corrections);
       this.recognition.stop();
-      this.parseSpeech();
+      this.parseSpeech_();
     }.bind(this);
     
     this.recognition.onerror = function(e) {
@@ -243,7 +190,11 @@ SpeechGames.Speech.prototype.startDictation = function() {
   }
 };
 
-SpeechGames.Speech.prototype.toggleDictation = function() {
+/**
+ * Toggles the listening from the microphone.
+ * @private
+ */
+SpeechGames.Speech.prototype.toggleDictation_ = function() {
   if (this.listening) {
     this.listening = !this.listening;
     this.recognition.stop();
@@ -254,11 +205,16 @@ SpeechGames.Speech.prototype.toggleDictation = function() {
     this.interval = null;
   } else {
     document.getElementById('microphone').src = this.mic_animate;
-    this.setMicInterval();
+    this.setMicInterval_();
   }
 };
 
-SpeechGames.Speech.prototype.checkHeyJerry = function() {
+/**
+ * Checks to see if 'Hey Jerry' has been said yet.
+ * @return {boolean} True if Hey Jerry has been said yet, else false.
+ * @private
+ */
+SpeechGames.Speech.prototype.checkHeyJerry_ = function() {
   if (this.awake || this.speech === "hey jerry" || (this.speech.includes("h") || this.speech.includes("j") || this.speech.includes("a"))) {
     this.awake = true;
     return true;
@@ -267,11 +223,12 @@ SpeechGames.Speech.prototype.checkHeyJerry = function() {
   }
 };
 
-SpeechGames.Speech.prototype.buildErrorMessage = function(e) {
-  return e.location !== undefined ? 'Line ' + e.location.start.line + ', column ' + e.location.start.column + ': ' + e.message : e.message;
-};
-
-SpeechGames.Speech.prototype.parseSpeech = function() {
+/**
+ * Parses the input and sends the result to SpeechGames.interpreter. Then outputs the response
+ * to the user interface.
+ * @private
+ */
+SpeechGames.Speech.prototype.parseSpeech_ = function() {
   this.previousRecognitionTime = Date.now();
   this.oldQ = $('#q').val();
   $('#parse-message').attr('class', 'message progress').text('Parsing the input...');
@@ -282,7 +239,7 @@ SpeechGames.Speech.prototype.parseSpeech = function() {
     this.speech = $('#q').val().toLowerCase();
     console.log(this.speech);
     if (!this.awake && this.demoMode) {
-      this.checkHeyJerry();
+      this.checkHeyJerry_();
       if (this.awake) {
         $('#user-message').hide().text("Awaiting your command!").fadeIn(200);
         $("#q").val("");
@@ -299,7 +256,7 @@ SpeechGames.Speech.prototype.parseSpeech = function() {
           .attr('class', 'message info')
           .text('Input parsed successfully.');
     $('#output').removeClass('disabled').text(jsDump.parse(this.output));
-    this.response = this.interpretSpeech();
+    this.response = this.interpretSpeech_();
     clearTimeout(this.timeout);
     this.result = true;
     $("#user-message").hide().text(this.response).fadeIn(200);
@@ -309,7 +266,7 @@ SpeechGames.Speech.prototype.parseSpeech = function() {
     if(e instanceof SpeechBlocks.UserError) {
       $('#user-message').text(e.message);
     } else {
-      $('#parse-message').attr('class', 'message error').text(this.buildErrorMessage(e));
+      $('#parse-message').attr('class', 'message error').text(this.buildErrorMessage_(e));
       if(this.speech !== '') {
         $('#user-message').hide().text('Sorry, I didn\'t understand \"' + this.speech + '\"').fadeIn(200);
         $("#q").val("");
@@ -323,45 +280,99 @@ SpeechGames.Speech.prototype.parseSpeech = function() {
   return result;
 };
 
-SpeechGames.Speech.prototype.interpretSpeech = function() {
+/**
+ * Calls the SpeechGames.interpreter's interpret function.
+ * @return {string} Response for user (i.e. 'Added a move block.').
+ * @private
+ */
+SpeechGames.Speech.prototype.interpretSpeech_ = function() {
   if (this.output !== null) {
     return SpeechGames.interpreter.interpret(this.output);
   }
 };
 
-SpeechGames.Speech.prototype.scheduleParse = function() {
-  if ($('#q').val() === this.oldQ) {
-    return;
+/**
+ * Schedule's the parsing to occur after a second of inactivity.
+ * @private
+ */
+SpeechGames.Speech.prototype.scheduleParse_ = function() {
+  if ($('#q').val() !== this.oldQ) {
+    if (this.parseTimer !== null) {
+      clearTimeout(this.parseTimer);
+      this.parseTimer = null;
+    }
+    this.parseTimer = setTimeout(function() {
+      this.parseSpeech_();
+      this.parseTimer = null;
+    }.bind(this), 1000);
   }
-
-  if (this.parseTimer !== null) {
-    clearTimeout(this.parseTimer);
-    this.parseTimer = null;
-  }
-
-  this.parseTimer = setTimeout(function() {
-    this.parseSpeech();
-    this.parseTimer = null;
-  }.bind(this), 1000);
 };
 
-// utility functions
-function createCode() {
+/**
+ * Builds an error message.
+ * @param {exception} e Exception from which to build the error message.
+ * @return {exception} The error message.
+ */
+SpeechGames.Speech.prototype.buildErrorMessage_ = function(e) {
+  return e.location !== undefined ? 'Line ' + e.location.start.line + ', column ' + e.location.start.column + ': ' + e.message : e.message;
+};
+
+/**
+ * Extracts a parameter from the URL.
+ * If the parameter is absent default_value is returned.
+ * @param {!string} name The name of the parameter.
+ * @param {!string} defaultValue Value to return if paramater not found.
+ * @return {string} The parameter value or the default value if not found.
+ */
+SpeechGames.getStringParamFromURL_ = function(name, defaultValue) {
+  var val =
+      window.location.search.match(new RegExp('[?&]' + name + '=([^&]+)'));
+  return val ? decodeURIComponent(val[1].replace(/\+/g, '%20')) : defaultValue;
+};
+
+/**
+ * Extracts a numeric parameter from the URL.
+ * If the parameter is absent or less than min_value, min_value is
+ * returned.  If it is greater than max_value, max_value is returned.
+ * @param {!string} name The name of the parameter.
+ * @param {!number} minValue The minimum legal value.
+ * @param {!number} maxValue The maximum legal value.
+ * @return {number} A number in the range [min_value, max_value].
+ */
+SpeechGames.getNumberParamFromURL_ = function(name, minValue, maxValue) {
+  var val = Number(SpeechGames.getStringParamFromURL_(name, 'NaN'));
+  return isNaN(val) ? minValue : goog.math.clamp(minValue, val, maxValue);
+};
+
+/**
+ * Generates code from the blockly workspace.
+ * @private
+ */
+SpeechGames.createCode_ = function() {
   Blockly.JavaScript.addReservedWords('code');
   return Blockly.JavaScript.workspaceToCode(SpeechGames.workspace);
-}
+};
 
-function showCode() {
+/**
+ * Shows the generated code.
+ * @private
+ */
+SpeechGames.showCode_ = function() {
   var modalEl = document.createElement('generatedCode');
   modalEl.style.width = '400px';
   modalEl.style.height = '300px';
   modalEl.style.margin = '100px auto';
   modalEl.style.backgroundColor = '#ff';
-  modalEl.textContent = createCode();
+  modalEl.textContent = createCode_();
   mui.overlay('on', modalEl);
-}
+};
 
-function getParameterByName(name, url) {
+/**
+ * Gets a parameter from the url.
+ * @param {!string} name Name of the param (i.e. debug)
+ * @param {string} url Optional url, otherwise window url is used.
+ */
+SpeechGames.getParameterByName_ = function(name, url) {
   if (!url) {
     url = window.location.href;
   }
@@ -371,4 +382,62 @@ function getParameterByName(name, url) {
   if (!results) return null;
   if (!results[2]) return '';
   return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
+};
+
+/**
+ * Initializes all of the SpeechGames objects and begins listening.
+ */
+$(document).ready(function() {
+  SpeechGames.speech = new SpeechGames.Speech();
+  SpeechGames.controller = SpeechBlocks.Controller.injectIntoDiv('blocklyDiv', {
+    media: 'lib/google-blockly/media/',
+    trashcan: false,
+    scrollbars: false,
+    toolbox: document.getElementById('toolbox')
+  });
+  SpeechGames.interpreter = new SpeechBlocks.Interpreter(SpeechGames.controller);
+  SpeechGames.workspace = SpeechGames.controller.workspace_;
+
+  if(!SpeechGames.getParameterByName_('debug')) {
+    $('#debug').hide();
+  }
+
+  if(!SpeechGames.getParameterByName_('demo')) {
+    SpeechGames.speech.awake = true;
+  } else {
+    SpeechGames.speech.demoMode = true;
+    SpeechGames.speech.awake = false;
+    SpeechGames.speech.setMicInterval_();
+    console.log("demo mode");
+  }
+  if (SpeechGames.getParameterByName_('level')) {
+    SpeechGames.speech.awake = true;
+  }
+   $('#q')
+    .change(SpeechGames.speech.scheduleParse_.bind(SpeechGames.speech))
+    .mousedown(SpeechGames.speech.scheduleParse_.bind(SpeechGames.speech))
+    .mouseup(SpeechGames.speech.scheduleParse_.bind(SpeechGames.speech))
+    .click(SpeechGames.speech.scheduleParse_.bind(SpeechGames.speech))
+    .keydown(SpeechGames.speech.scheduleParse_.bind(SpeechGames.speech))
+    .keyup(SpeechGames.speech.scheduleParse_.bind(SpeechGames.speech))
+    .keypress(SpeechGames.speech.scheduleParse_.bind(SpeechGames.speech));
+
+  $('#microphone')
+      .click(SpeechGames.speech.toggleDictation_.bind(SpeechGames.speech));
+  if (SpeechGames.speech.demoMode && !SpeechGames.speech.awake) {
+     $("#user-message").hide().text("Say 'Hey Jerry' to wake me up.").fadeIn(500);
+  } else {
+    $("#user-message").hide().text("Awaiting your command!").fadeIn(500);
+  }
+
+  // $('#runButton').on('click', run);
+  // $('#showButton').on('click', showCode_);
+
+  $('#debugButton').on('click', function() { 
+    $('#debug').toggle();
+  });
+  $('#buttonRow').hide();
+  $('#levelDescription').text(Turtle.descriptions[SpeechGames.LEVEL]);
+  
+  SpeechGames.LEVEL = SpeechGames.getNumberParamFromURL_('level', 1, SpeechGames.MAX_LEVEL);
+});
