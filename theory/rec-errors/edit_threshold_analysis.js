63aa99aecde580febfa362e3b71bbc71f47554b8
sahibgoa <pandori@wisc.edu>
2017-10-16 14:39:14 -0500

    trying analysis using https

---
+++ theory/rec-errors/edit_threshold_analysis.js
@@ -0,0 +1,75 @@
+/**
+  * Use positive and negative values to find an optimal value for the Maximum
+  * edit threshold (for phonemes)
+  * @author Sahib Singh Pandori <pandori@wisc.edu>
+  */
+
+// AnalyzeThreshold.THRESHOLD_INCREMENT = 0.01;
+
+$(document).ready(function() {
+    $.ajax({
+        type: "GET",
+        url: "data.csv",
+        dataType: "text",
+        success: function(data) {processData(data);}
+     });
+});
+
+/**
+  * Reads a given CSV file and returns a list of attributes of each row
+  * @param filepath - the csv file to be read
+  * @return array of array of attributes of each row
+  */
+function processData(allText) {
+  var BLOCK_IDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  var VALUE_SETS = [
+      ['up', 'down'],
+      ['left', 'right'],
+      ['45', '72', '90', '120', '144'],
+      ['2', '3', '4', '5'],
+      ['red', 'orange', 'blue', 'green', 'yellow', 'purple', 'brown', 'black', 'white']];
+  var BLOCK_TYPES = ['move', 'turn', 'pen', 'color', 'repeat'];
+  var THRESHOLD_INCREMENT = 0.01;
+
+  // Get all lines and header
+  var allTextLines = allText.split(/\r\n|\n/);
+  var headers = allTextLines[0].split(',');
+  var results = [];
+
+  // For each value of threshold go over all lines and calculate accuracy
+  for (var i = 0; i <= (1.0/THRESHOLD_INCREMENT); i++) {
+    // Set the threshold to current value
+    var corrector = Corrector();
+    corrector.MAX_MODIFICATION = i*THRESHOLD_INCREMENT;
+
+    // Calculate the accuracy with current threshold
+    var numCorrect = 0;
+    for (var i = 1; i < allTextLines.length; i++) {
+      var data = allTextLines[i].split(',');
+      var cor = corrector.correct(data[1], BLOCK_IDS, VALUE_SETS, BLOCK_TYPES);
+      var accepted = data[1] != cor;
+      var correct = data[0] == cor;
+      if ((correct && accepted) || (!correct && !accepted)) {
+        numCorrect++;
+      }
+    }
+
+    // Store the accuracy result
+    results.push(numCorrect/allTextLines.length);
+  }
+
+  console.log(results);
+}
+
+
+// function AnalyzeThreshold() {
+//   this.curr_threshold = AnalyzeThreshold.THRESHOLD_INCREMENT;
+//   while (this.curr_threshold < 1) {
+//     // TODO: Read the csv file for recognition and correction
+//     var lines = this.readCSV();
+//     console.log(lines);
+//     // TODO: Loop through them and get accuracy at each threshold
+//     // TODO: Print the accuracy for each one (and optimal threshold)
+//     this.curr_threshold += AnalyzeThreshold.THRESHOLD_INCREMENT;
+//   }
+// }
