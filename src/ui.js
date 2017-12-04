c381bd3219ed124a89dc4cdf371e7246b6b22f8b
David Liang <davidliangx27@gmail.com>
2017-12-04 16:33:51 -0600

    formatting of one document

--- src/ui.js
+++ src/ui.js
@@ -1,64 +1,72 @@
+/**
+ * @fileoverview Controls UI and suggestions.
+ * @author aravart@cs.wisc.edu (Ara Vartanian)
+ * @author dliang@cs.wisc.edu (David Liang)
+ * @author pandori@wisc.edu (Sahib Singh Pandori)
+ * @author ehernandez4@wisc.edu (Evan Hernandez)
+ */
+
 $(document).ready(function() {
 
-var title = goog.dom.$('levels')
-var debugFlag = '';
-if(SpeechGames.getParameterByName_('debug')) {
-  debugFlag = "&?debug=1";
-}
-for (var i = 0; i < SpeechGames.MAX_LEVEL; i++) {
-  if(i == SpeechGames.LEVEL - 1) {
-    var levelElement = goog.dom.createDom('span', {
-      'class': 'level_number level_done', 
-      'id': 'level' + (i + 1), 
-      'href': '?level=' + (i + 1) + debugFlag}, 
-      (i + 1).toString());    
-  } else {
-    var levelElement = goog.dom.createDom('a', {
-      'class': (i == SpeechGames.MAX_LEVEL - 1) ? 'level_number' : 'level_dot',
-      'id': 'level' + (i + 1), 
-      'href': '?level=' + (i + 1)  + debugFlag},
-      (i == SpeechGames.MAX_LEVEL - 1) ? (i+1).toString() : '');
+  var title = goog.dom.$('levels')
+  var debugFlag = '';
+  if(SpeechGames.getParameterByName_('debug')) {
+    debugFlag = "&?debug=1";
   }
-  goog.dom.appendChild(title, levelElement);
-  goog.dom.appendChild(title, goog.dom.createTextNode(' '));
-}
-
-var sug = new SpeechGames.Suggestions();
-sug.setSuggestions(['get']); // Initially, suggest adding a block.
-SpeechGames.controller.addStateChangeListener(function(state) {
-  var sugs = ['get']; // We always suggest adding blocks.
-  if (state.empty) {
-    sug.setSuggestions(sugs); 
-    return;
-  }
-  
-  if (!state.allBlocksConnected) {
-    sugs.push('connect_under');
-  }
-  
-  var blocks = SpeechGames.workspace.getAllBlocks();
-  // has repeat block type
-  var hasRepeat = false;
-  for (var i = 0; i < blocks.length; i++) {
-    if (blocks[i].type == 'turtle_repeat_internal') {
-      hasRepeat = true;
-    } 
+  for (var i = 0; i < SpeechGames.MAX_LEVEL; i++) {
+    if(i == SpeechGames.LEVEL - 1) {
+      var levelElement = goog.dom.createDom('span', {
+        'class': 'level_number level_done', 
+        'id': 'level' + (i + 1), 
+        'href': '?level=' + (i + 1) + debugFlag}, 
+        (i + 1).toString());    
+    } else {
+      var levelElement = goog.dom.createDom('a', {
+        'class': (i == SpeechGames.MAX_LEVEL - 1) ? 'level_number' : 'level_dot',
+        'id': 'level' + (i + 1), 
+        'href': '?level=' + (i + 1)  + debugFlag},
+        (i == SpeechGames.MAX_LEVEL - 1) ? (i+1).toString() : '');
+    }
+    goog.dom.appendChild(title, levelElement);
+    goog.dom.appendChild(title, goog.dom.createTextNode(' '));
   }
 
-  if (hasRepeat && blocks.length >= 2) {
-    sugs.push('connect_inside');
-  }
+  var sug = new SpeechGames.Suggestions();
+  sug.setSuggestions(['get']); // Initially, suggest adding a block.
+  SpeechGames.controller.addStateChangeListener(function(state) {
+    var sugs = ['get']; // We always suggest adding blocks.
+    if (state.empty) {
+      sug.setSuggestions(sugs); 
+      return;
+    }
+    
+    if (!state.allBlocksConnected) {
+      sugs.push('connect_under');
+    }
+    
+    var blocks = SpeechGames.workspace.getAllBlocks();
+    // has repeat block type
+    var hasRepeat = false;
+    for (var i = 0; i < blocks.length; i++) {
+      if (blocks[i].type == 'turtle_repeat_internal') {
+        hasRepeat = true;
+      } 
+    }
 
-  if (!state.modifiableBlockIds.isEmpty()) {
-    sugs.push('change');
-  }
+    if (hasRepeat && blocks.length >= 2) {
+      sugs.push('connect_inside');
+    }
 
-  // When the workspace is nonempty, we always suggest deleting blocks
-  // and running the program.
-  sugs.push('delete');
-  sugs.push('run');
+    if (!state.modifiableBlockIds.isEmpty()) {
+      sugs.push('change');
+    }
 
-  sug.setSuggestions(sugs);
-});
+    // When the workspace is nonempty, we always suggest deleting blocks
+    // and running the program.
+    sugs.push('delete');
+    sugs.push('run');
+
+    sug.setSuggestions(sugs);
+  });
 
 });
